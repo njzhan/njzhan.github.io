7578585843ea3e29cba6df1a577f7e42454b2cab
njzhan <njzhan@pku.edu.cn>
2026-08-16 07:34:23 +0000

    Deploying to gh-pages from @ njzhan/njzhan.github.io@793612873226f37a36f2a66c009657f3f54dbc7a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/Publications/";
           },
+        },{id: "nav-talks",
+          title: "Talks",
+          description: "Invited talks, keynotes and tutorials, in reverse chronological order.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Talks/";
+          },
         },{id: "nav-academic-service",
           title: "Academic Service",
           description: "Professional activities and academic service roles.",
